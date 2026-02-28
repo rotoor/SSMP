229d422835f6a4d0154db41c40c2cae3f42b526d
rotoor <69124427+rotoor@users.noreply.github.com>
2026-02-28 19:59:20 +0200

    73

--- LR1/Task1/73.py
+++ LR1/Task1/73.py
@@ -1,5 +1,10 @@
-for i in range(3):
+'''for i in range(3):
     n = int(input())
     h = (n // 60) % 24
     m = n % 60
     print(h, m)
+    Переплутав з 63'''
+for i in range(3):
+    n = int(input())
+    r = hex(n)[2:]
+    print(r)
